c99a92e82efdcc9ef3d778542643200255e4b0f7
Michael Lee <michael.jw.lee@gmail.com>
2019-06-12 14:31:29 -0400

    Added configuration screen UI

---
+++ app/src/main/java/com/example/spacetraders/data/ConfigScreen.java
@@ -0,0 +1,13 @@
+package com.example.spacetraders.data;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_screen);
+    }
+}
