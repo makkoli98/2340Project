d569d842a7e6901f7e2033ff71f1e3286e42f608
Vincent Huang <huangvincent170@gmail.com>
2019-06-12 21:52:44 -0400

    added R import statement

--- app/src/main/java/com/example/spacetraders/data/ConfigScreen.java
+++ app/src/main/java/com/example/spacetraders/data/ConfigScreen.java
@@ -3,6 +3,8 @@ package com.example.spacetraders.data;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.spacetraders.R;
+
 public class ConfigScreen extends AppCompatActivity {
 
     @Override
