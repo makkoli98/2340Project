a6496272acbb0b6879c071eed92605a750b5fcfa
Banj <nikbanj9@gmail.com>
2019-07-09 22:18:15 -0400

    Added changes to the ShipYard UI

--- app/src/main/java/com/example/spacetraders/data/views/ShipYardActivity.java
+++ app/src/main/java/com/example/spacetraders/data/views/ShipYardActivity.java
@@ -2,16 +2,26 @@ package com.example.spacetraders.data.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.spacetraders.R;
 import com.example.spacetraders.data.models.Interactor;
 import com.example.spacetraders.data.entity.Character;
 
-public class ShipYardActivity extends AppCompatActivity {
+public class ShipYardActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Character character1;
     private TextView viewCurrency;
+    private Button previewButton;
+    private Button purchaseButton;
+    private Spinner shipSpinner;
+
 
 
     @Override
@@ -21,9 +31,30 @@ public class ShipYardActivity extends AppCompatActivity {
 
         character1 = Interactor.getInteractor().getCharacter();
         viewCurrency = findViewById(R.id.playerCurrency);
+        previewButton = findViewById(R.id.previewButton);
+        purchaseButton = findViewById(R.id.purchaseButtn);
+        shipSpinner = findViewById(R.id.shipSpinner);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ship_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        shipSpinner.setAdapter(adapter);
+        shipSpinner.setOnItemSelectedListener(this);
+
 
         viewCurrency.setText("Credits: " + character1.getCurrency() + "c");
 
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
