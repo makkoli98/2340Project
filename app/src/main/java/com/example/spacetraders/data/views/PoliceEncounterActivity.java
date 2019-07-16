acb6c684d414b07993f2a4dfa7e28a1437ef7791
Michael Lee <michael.jw.lee@gmail.com>
2019-07-16 11:20:18 -0400

    Added views for each of the encounters

---
+++ app/src/main/java/com/example/spacetraders/data/views/PoliceEncounterActivity.java
@@ -0,0 +1,15 @@
+package com.example.spacetraders.data.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.spacetraders.R;
+
+public class PoliceEncounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_police_encounter);
+    }
+}
