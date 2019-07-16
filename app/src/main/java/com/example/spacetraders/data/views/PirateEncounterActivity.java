64ecf6611e0f860853053bf6382af3f5df090c43
Michael Lee <michael.jw.lee@gmail.com>
2019-07-16 12:50:56 -0400

    Traveling between systems now randomly takes you to encounter activity

--- app/src/main/java/com/example/spacetraders/data/views/PirateEncounterActivity.java
+++ app/src/main/java/com/example/spacetraders/data/views/PirateEncounterActivity.java
@@ -1,15 +1,25 @@
 package com.example.spacetraders.data.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.spacetraders.R;
 
 public class PirateEncounterActivity extends AppCompatActivity {
 
+    Button escapeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pirate_encounter);
+
+        escapeButton = findViewById(R.id.escape_pirate);
+        escapeButton.setOnClickListener((view) -> {
+            startActivity(new Intent(PirateEncounterActivity.this, MainGameActivity.class));
+            finish();
+        });
     }
 }
--- app/src/main/java/com/example/spacetraders/data/views/TraderEncounterActivity.java
+++ app/src/main/java/com/example/spacetraders/data/views/TraderEncounterActivity.java
@@ -1,15 +1,25 @@
 package com.example.spacetraders.data.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.spacetraders.R;
 
 public class TraderEncounterActivity extends AppCompatActivity {
 
+    Button escapeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trader_encounter);
+
+        escapeButton = findViewById(R.id.escape_trader);
+        escapeButton.setOnClickListener((view) -> {
+            startActivity(new Intent(TraderEncounterActivity.this, MainGameActivity.class));
+            finish();
+        });
     }
 }
