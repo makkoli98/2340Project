91ed56d59e52adba24d8531fbf3388ddbc42163b
sveludhandi <50715193+sveludhandi@users.noreply.github.com>
2019-07-16 22:19:04 -0400

    Create SandeepTest.java

---
+++ app/src/test/java/com/example/spacetraders/SandeepTest.java
@@ -0,0 +1,25 @@
+package com.example.spacetraders;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.example.spacetraders.data.entity.GameDifficulty;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class SandeepTest {
+    @Test
+    public SolarSystem() {
+        //assertEquals(4, 2 + 2);
+    }
+    
+    /*
+    @Test
+    public static int getRandom(double[] weights){
+    
+    }
+}
