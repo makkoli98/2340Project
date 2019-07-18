68bbf9971e5a0ed25a3ba2d1433a5e30e9072fd3
Banj <nikbanj9@gmail.com>
2019-07-17 23:36:20 -0400

    Committing a merge issue

--- app/src/test/java/com/example/spacetraders/SandeepTest.java
+++ app/src/test/java/com/example/spacetraders/SandeepTest.java
@@ -5,6 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import com.example.spacetraders.data.entity.GameDifficulty;
+import com.example.spacetraders.data.entity.SolarSystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.HashSet;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,13 +19,42 @@ import com.example.spacetraders.data.entity.GameDifficulty;
  */
 public class SandeepTest {
     @Test
-    public SolarSystem() {
-        //assertEquals(4, 2 + 2);
+    public void testName() {
+        HashSet<String> solarSystemNames = new HashSet<>();
+        try {
+            for (int i = 0; i < 10; i++) {
+                SolarSystem solarSystem = new SolarSystem(GameDifficulty.BEGINNER);
+                solarSystemNames.add(solarSystem.getName());
+            }
+            assertEquals(solarSystemNames.size(), 10);
+        } catch (Exception e) {
+
+        }
     }
-    
-    /*
+
     @Test
-    public static int getRandom(double[] weights){
-    
+    public void testNumberSystems() {
+        try {
+            for (int i = 0; i < 200; i++) {
+                SolarSystem solarSystem = new SolarSystem(GameDifficulty.BEGINNER);
+            }
+        } catch (Exception e) {
+            //System.out.println(e.getMessage());
+            assertEquals(e.getMessage(), "Max Solar System Limit Reached");
+        }
+    }
+
+    @Test public void testRandom() {
+        double[] weights = new double[] {.1, .2, .15, .3, .25};
+        double[] indices = new double[] {0,0,0,0,0};
+        for (int i = 0; i < 1000; i++) {
+            int index = SolarSystem.getRandom(weights);
+            indices[index]++;
+        }
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < indices.length; i++) {
+                assertTrue(indices[i]/1000 <= weights[i]* 1.2 || indices[i]/1000 >= weights[i] * .8);
+            }
+        }
     }
 }
